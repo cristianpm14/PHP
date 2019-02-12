70015adb227b6d0895e479197da4884287a24747
cristianpm14 <43580485+cristianpm14@users.noreply.github.com>
2019-02-12 19:59:09 +0100

    actividad

---
+++ ejer-20a.php
@@ -0,0 +1,24 @@
+<html>
+<head></head>
+<body>
+
+<?php
+$con=mysqli_connect("localhost","cristian","258456","base1");
+$id = $_REQUEST["curso"];
+
+$sql="SELECT nombrecurso from cursos where nombrecurso='".$id."'";
+$rs=mysqli_query($con,$sql);
+
+if (mysqli_num_rows($rs)==0) {
+	echo "El nombre del curso no existe.";
+} 
+else {
+	mysqli_query($con,"delete from cursos where nombrecurso='".$id."'");
+	echo "Curso Eliminado.";
+}
+mysqli_close($con);
+?>
+
+
+</body>
+</html>
---
+++ formu20a.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<form action="ejer-20a.php" method="post">
+Nombre del Curso: <input type="text" name="curso">
+<input type="submit">
+</form>
+</body>
+</html>
