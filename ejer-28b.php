83b8100ce5b045f346db5cc55b925b133ab2057c
cristianpm14 <43580485+cristianpm14@users.noreply.github.com>
2019-03-09 17:23:01 +0100

    act

--- ejer-28b.php
+++ ejer-28b.php
@@ -1,11 +1,22 @@
 <html>
-<head> <title>Subir archivo</title> </head>
+<head> <title>Agregar articulo</title> </head>
 <body>
-<h1>Subir archivo</h1>
-<form action="upload-28a.php" method="post" enctype="multipart/form-data">
+<h1>Agregar articulo</h1>
+<form action="upload-28b.php" method="post" enctype="multipart/form-data">
     Nombre del articulo: <input type="text" name="nom"><br>
     Descripcion del articulo: <input type="text" name="des"><br>
-    Precio: <input type="text" name="pre"><br>
+    Precio: <input type="double" name="pre"><br>
+    Bargain: <input type="text" name="bar"><br>
+    Categoria: <select name="cate">
+        <?php
+        $con=mysqli_connect("localhost","cristian","258456","shop");
+        $sql="select * from category";
+        $rs=mysqli_query($con,$sql);
+        while ($row=mysqli_fetch_array($rs)) {
+            echo "<option value=\"$row[code]\">$row[name]</option>";
+        }
+        ?>
+        </select><br>
     Selecciona archivo: <input type="file" name="foto"><br>
     <input type="submit" value="Upload">
 </form>
--- upload-28b.php
+++ upload-28b.php
@@ -1,3 +1,29 @@
 <?php
+$con=mysqli_connect("localhost","cristian","258456","shop");
+$nombre=$_REQUEST["nom"];
+$descrip=$_REQUEST["des"];
+$precio=$_REQUEST["pre"];
+$bargain=$_REQUEST["bar"];
+$cate=$_REQUEST["cate"];
+$sql="insert into article (name,description,price,categorycode,bargain) values ('$nombre','$descrip','$precio','$cate','$bargain')";
+echo "Se agrego el articulo";
+echo "<br>";
+if ($con->query($sql) === TRUE){
+    $last_id = $con->insert_id;
+    echo "Articulo añadido";
+    echo "<br>";
+    copy($_FILES['foto']['tmp_name'],$_FILES['foto']['name']);
+    echo "Archivo añadido";
+    echo "<br>";
+    $name=$_FILES['foto']['name'];
+    echo "<img src=\"$name\">";
+    echo "<br>";
 
+    $file_name="Foto".$last_id;
+    move_uploaded_file($_FILES["foto"]["tmp_name"], "F:/wamp64/www/Ejercicios/{$file_name}.png");
+    echo $file_name;
+}
+else {
+    echo "Error: ".$sql."<br>".$con->error;
+}
 ?>
