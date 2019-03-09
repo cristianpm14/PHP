c307f73765c61fbfdae29536636fefc1ddb57eef
cristianpm14 <43580485+cristianpm14@users.noreply.github.com>
2019-03-09 16:33:27 +0100

    act

---
+++ ejer-28b.php
@@ -0,0 +1,13 @@
+<html>
+<head> <title>Subir archivo</title> </head>
+<body>
+<h1>Subir archivo</h1>
+<form action="upload-28a.php" method="post" enctype="multipart/form-data">
+    Nombre del articulo: <input type="text" name="nom"><br>
+    Descripcion del articulo: <input type="text" name="des"><br>
+    Precio: <input type="text" name="pre"><br>
+    Selecciona archivo: <input type="file" name="foto"><br>
+    <input type="submit" value="Upload">
+</form>
+</body>
+</html>
--- upload-28a.php
+++ upload-28a.php
@@ -11,7 +11,7 @@ $name2=$_FILES['foto2']['name'];
 $name3=$_FILES['foto3']['name'];
 echo "<p><img src=\"$name\"></p>";
 echo "<p><img src=\"$name2\"></p>";
-echo "<img src=\"$name3\"></p>";
+echo "<p><img src=\"$name3\"></p>";
 ?>
 </body> 
 </html>
